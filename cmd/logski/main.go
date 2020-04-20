62d1bf626aa3f4a283856332a17190898a350a32
modusbecker <gareth@dotmodus.com>
2020-04-20 13:44:12 +0200

    Added filtering to the namespaces

--- cmd/logski/main.go
+++ cmd/logski/main.go
@@ -18,10 +18,22 @@ func main() {
 
 	namespace := flag.String("n", "default", "the k8s namespace to access")
 	filter := flag.String("f", "", "the filter for pods")
-
 	client := k8s.CreateClient()
+	
+	fmt.Printf("\tNamespace::%s\n", *namespace)
+	fmt.Printf("\tFilter::%s\n", *filter)
+
+	var namespaceString string
+	if (*namespace != "") {
+		namespaces, err := k8s.GetNameSpaces(client, *namespace)
+		if (err != nil) {
+			panic(err)
+		}
+		namespaceString = namespaces[0]
+		fmt.Println(fmt.Sprintf("Now using name space %s", namespaceString))
+	}
 	for {
-		pods := k8s.GetPods(client, *namespace, 20, *filter)
+		pods := k8s.GetPods(client, namespaceString, 20, *filter)
 		if len(pods) == 0 {
 			fmt.Println("--> No pods found")
 			fmt.Println("--> Trying again in 3 seconds")
@@ -32,7 +44,7 @@ func main() {
 			time.Sleep(1 * time.Second)
 			continue
 		}
-		fmt.Printf("Latest Pods in %s\n", *namespace)
+		fmt.Printf("Latest Pods in %s\n", namespace)
 		for index, pod := range pods {
 			fmt.Printf("%d. %s | %s \n", index+1, pod.Name, pod.Time.Format(time.RFC3339))
 		}
@@ -56,7 +68,7 @@ func main() {
 		}
 
 		podItem := pods[podID-1]
-		logs := k8s.GetPodLogs(client, *namespace, podItem.Name)
+		logs := k8s.GetPodLogs(client, namespaceString, podItem.Name)
 		fmt.Println(logs)
 	}
 }
